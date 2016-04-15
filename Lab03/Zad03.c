bd83da673fb3d54e5803135f5f04e1906a4511ad
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-04-15 10:56:52 +0200

    Update Zad03.c

--- Lab03/Zad03.c
+++ Lab03/Zad03.c
@@ -4,11 +4,22 @@
 
 int main()
 {
-	printf("FLT_MAX DO (int)FLT_MAX:\n %e DO %i \n", FLT_MAX, (int)FLT_MAX);
-	// najbli¿szy floatowi max int
-	printf("DBL_MAX DO (int)DBL_MAX:\n %e DO %i \n", DBL_MAX, (int)DBL_MAX);
-	// najbli¿szy double max int
-	printf("LDBL_MAX DO (double)LDBL_MAX:\n %Le DO %e \n", LDBL_MAX, (double)LDBL_MAX);
-	// wynik = 'infinity'
+  printf("FLT_MAX -> (int)FLT_MAX:\n%e -> %i\n\n", FLT_MAX, (int)FLT_MAX);
+  //wyznacza maksymalna (czyli jak najblizsza zadanej(float)) liczbe w innym typie (int)
+  printf("INT_MAX -> (float)INT_MAX:\n%i -> %e\n\n", INT_MAX, (float)INT_MAX);
+  //wypisuje maksymalna wartosc inta (float > int)
+  printf("DBL_MAX -> (int)DBL_MAX:\n%e -> %i\n", DBL_MAX, (int)DBL_MAX);
+  //wyznacza maksymalna (czyli jak najblizsza zadanej(double)) liczbe w innym typie (int)
+  printf("Wartosc double max wychodzi poza maksymalna wartosc inta, zatem drukowana jest wartosc maksymalna inta.\n\n");
+  printf("INT_MAX -> (double)INT_MAX:\n%i -> %e\n\n", INT_MAX, (double)INT_MAX);
+  //wypisuje maksymalna wartosc inta (double > int)
+  printf("LDBL_MAX -> (double)LDBL_MAX:\n%Le -> %e\n\n", LDBL_MAX, (double)LDBL_MAX);
+  printf("(double)LDBL_MAX podzielone przez 2:\n %e \n\n", (double)LDBL_MAX/2);
+  printf("(double)LDBL_MAX podzielone przez 4:\n %e \n\n", (double)LDBL_MAX/4);
+  //wypisuje infinity
+  printf("LONG_MAX -> (float)LONG_MAX:\n%li -> %e\n\n", LONG_MAX, (float)LONG_MAX);
+  //wypisuje maksymalna wartosc longa(float > long)
+  printf("INT_MAX -> (float)INT_MAX:\n%i -> %e\n\n", INT_MAX, (float)INT_MAX);
+  //wypisuje maksymalna wartosc inta (float > int)
   return 0;
 }
