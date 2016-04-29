d677403e821104639e593cb6368b72dab2498a99
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-04-29 11:16:43 +0200

    Update Zad03.c

--- Lab01/Zad03.c
+++ Lab01/Zad03.c
@@ -8,24 +8,26 @@ int main(){
   for(j=0; j<5; j++){
     printf("%d", a1[j]);
   }
+  printf ("\n");
   a2[i++] = 0;
   j=0; i=0;
   for(j=0; j<5; j++){
     printf("%d", a2[j]);
   }
+  printf ("\n");
   int y, x;
   printf("y=(x += 2)\n");
   printf("Podaj x: ");
   scanf("%d", &x);
   y=(x += 2);
   printf("Wynik: %d\n", y);
-  
+
   int z, c;
   printf("z = (c %%= 3) + (c  %%= 4)\n");
   printf("Podaj c: ");
-  scanf("%d", &x);
-  z = (c %= 3) + (c %= 4);
+  scanf("%d", &c);
+  z = (c % 3) + (c % 4);
   printf("Wynik: %d\n", z);
 
-return 0;  
+return 0;
 }
