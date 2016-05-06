444f0f453e04893c6ecc206c9cd5f2843d52ee78
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-06 11:11:58 +0200

    Update Zad01.c

--- Lab07/Zad01.c
+++ Lab07/Zad01.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[ ]) {
-    int i;
-    for(i=argc-1; i>0; i--){
-        printf("%s ", argv[i]);
-        }
+  int i;
+  for(i = argc - 1; i > 0; i--){
+    printf("%s ", argv[i]);
+    }
 printf("\n");
-return 0;
 }
