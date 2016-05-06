b9839cbf3212f35bdab14a60300d4e02748b337f
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-06 10:21:59 +0200

    Update Zad02.c

--- Lab07/Zad02.c
+++ Lab07/Zad02.c
@@ -1,71 +1,130 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX_DL_IM 11
-#define MAX_DL_NA 17
-#define DUZO 10000
-
-typedef struct osoba {
-  char imie[MAX_DL_IM];
-  char nazwisko[MAX_DL_NA];
+#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+#define IMIE_MAX 20
+#define NAZW_MAX 20
+#define IL_OSOB 10000
+typedef struct
+{
+  char imie[IMIE_MAX+1];
+  char nazwisko[NAZW_MAX+1];
   int pensja;
 } osoba;
-
-osoba tabl[DUZO];
-
-int czytanie(char plik1[]){
-        int linie = 0;
+osoba spis[IL_OSOB];
+//=======================================================
+void utworz_spis(char *name, int *linie)
+{
+  FILE *baza = fopen(name, "r");
+  if(baza == NULL) printf("\nCOS SIE ZEPSULO!\n\n");
+  int i;
+  for(i = 0; i < *linie; i++)
+  {
+    fscanf(baza, "%s", spis[i].imie);
+    fscanf(baza, "%s", spis[i].nazwisko);
+    fscanf(baza, "%i", &spis[i].pensja);
+  }
+  fclose(baza);
+}
+//=======================================================
+int lines(char *name)
+{
+  int linie = 0;
   FILE* plik;
-        plik = fopen(plik1, "r");
-        int w;
-        do{
-                fscanf(plik, "%s", tabl[linie].imie);
-                fscanf(plik, "%s", tabl[linie].nazwisko);
-        w =     fscanf(plik, "%i", &tabl[linie].pensja);
-        linie++;
-        }while(w  != EOF);
-        fclose(plik);
-        return linie-1;
+  plik = fopen(name, "r");
+  int w;
+  do
+  {
+    fscanf(plik, "%s", spis[linie].imie);
+    fscanf(plik, "%s", spis[linie].nazwisko);
+    w = fscanf(plik, "%i", &spis[linie].pensja);
+    linie++;
+  } while(w != EOF);
+  fclose(plik);
+  return linie-1;
 }
-
-void pisanie(char plik1[], int ile){
-        FILE* plik;
-        plik = fopen(plik1, "w");
-        int i;
-        for(i=0;i<ile;i++){
-        fprintf(plik, "%s \t", tabl[i].imie);
-        fprintf(plik, "%s \t", tabl[i].nazwisko);
-        fprintf(plik, "%i \n", tabl[i].pensja);
-        }
-}
-int poprzedza (osoba os1, osoba os2)
+//=======================================================
+int compare(const void *a, const void *b)
 {
-  if (strcmp(os1.nazwisko, os2.nazwisko) < 0) return 1;
-  else if (strcmp(os1.nazwisko, os2.nazwisko) > 0 ) return 0;
-  else return (strcmp(os1.imie, os2.imie) < 0);
+  osoba *osoba_a, *osoba_b;
+  osoba_a = (osoba *) a;
+  osoba_b = (osoba *) b;
+  return strcmp(osoba_a->nazwisko, osoba_b->nazwisko);
 }
-void sortowanie ( int ile, osoba tabl[])
+//=======================================================
+void sort_qsort(int *linie)
 {
-    int i, j;
-    for (i = ile; i > 1; i--)
-    for (j = 1; j < i-1; j++)
-      if(poprzedza(tabl[j], tabl[j-1]))
+  qsort(spis, *linie, sizeof(osoba), compare);
+  FILE *baza_q = fopen("qsort.txt", "w");
+  int i;
+  for(i = 0; i < *linie; i++)
+  {
+    fprintf(baza_q, "%20s ", spis[i].imie);
+    fprintf(baza_q, "%20s ", spis[i].nazwisko);
+    fprintf(baza_q, "%20i\n", spis[i].pensja);
+  }
+  fclose(baza_q);
+}
+//=======================================================
+void sort_bubble(int *linie)
+{
+  int i, j;
+  char temp[NAZW_MAX+1];
+  for(i = 1; i <= *linie; i++)
+  {
+    for(j = i;j < *linie; j++)
+    {
+      if(strcmp(spis[j-1].nazwisko, spis[j].nazwisko) > 0)
       {
-        osoba pom = tabl[j];
-        tabl[j] = tabl[j-1];
-        tabl[j-1] = pom;
+        strcpy(temp, spis[j-1].nazwisko);
+        strcpy(spis[j-1].nazwisko, spis[j].nazwisko);
+        strcpy(spis[j].nazwisko, temp);
       }
+      else if(strcmp(spis[j-1].nazwisko, spis[j].nazwisko) == 0)
+      {
+        strcpy(temp, spis[j-1].imie);
+        strcpy(spis[j-1].imie, spis[j].imie);
+        strcpy(spis[j].imie, temp);
+      }
+    }
+  }
+  FILE* baza_b = fopen("bubblesort.txt", "w");
+  for(i = 0; i < *linie; i++)
+  {
+    fprintf(baza_b, "%20s ", spis[i].imie);
+    fprintf(baza_b, "%20s ", spis[i].nazwisko);
+    fprintf(baza_b, "%20i\n", spis[i].pensja);
+  }
+  fclose(baza_b);
 }
-
-int main (int arg_num, char* arg[]){
-        if(arg_num == 3){
-        int ile = czytanie(arg[1]);
-        if(ile > 0){
-        sortowanie(ile, tabl);
-        pisanie(arg[2],ile);
-        }
-        }
-        else {
-printf(" blad \n");
-}
-        return 0;
+//=======================================================
+int main(int args, char* arg[])
+{
+  if(args == 2)
+  {
+    char name[100];
+    sscanf(arg[1], "%s", name);
+    int l;
+    l = lines(name);
+    if(l > IL_OSOB)
+    {
+      printf("Za duzo osob! Nie dam rady!\n");
+      return 0;
+    }
+    printf("Zeskanowalem %i linii z pliku %s\n", l, arg[1]);
+    utworz_spis(name, &l);
+    clock_t begin = clock();
+    sort_qsort(&l);
+    clock_t end = clock();
+    printf("\nCzas wykonania qsorta: %lf sek.\n\n", (double)(end-begin)/CLOCKS_PER_SEC);
+    clock_t begin2 = clock();
+    sort_bubble(&l);
+    clock_t end2 = clock();
+    printf("\nCzas wykonania bubble: %lf sek.\n\n", (double)(end2-begin2)/CLOCKS_PER_SEC);
+    printf("\nPosortowane!\n\n");
+    return 0;
+  } else printf("\nNiepoprawne wywolanie programu!\n");
+  printf("Wywolanie programu wymaga argumentu!\n");
+  printf("./zad2b nazwapliku(lub lokalizacja pliku)!\n");
 }
