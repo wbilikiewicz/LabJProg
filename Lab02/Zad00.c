aae5a3c2e375aa3f9fe618e321d0e00a60e9c1c2
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-13 10:35:59 +0200

    Update Zad00.c

--- Lab02/Zad00.c
+++ Lab02/Zad00.c
@@ -1,6 +1,13 @@
+/* Jakie będą wyniki następujących działań?
+    * 5&3
+    * 5|3
+    * 7<<2&7
+    * 7<<(2&7)
+    * ((-1)<<8)>>16
+    * 13^9 */
+    
 #include <stdio.h>
-int main ()
-{
+int main () {
   printf ("%d\n", 23&15);
   printf ("%d\n", (-8)>>2);
   printf ("%d\n", 5&3);
@@ -9,5 +16,4 @@ int main ()
   printf ("%d\n", 7<<(2&7));
   printf ("%d\n", ((-1)<<8)>>16);
   printf ("%d\n", 13^9);
-  return 0;
 }
