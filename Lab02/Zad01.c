05e2f26c7c23e1b799f94274a10e8507dd598e64
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-13 10:37:13 +0200

    Update Zad01.c

--- Lab02/Zad01.c
+++ Lab02/Zad01.c
@@ -1,29 +1,25 @@
+// Napisać program, który wczytuje tekst aż do EOF, a następnie drukuje bitowe obrazy wczytanych znaków. 
+
 #include <stdio.h>
 
-int main ()
-{
+int main () {
   FILE *fp;
   char znak;
   int c = 0;
   int k, i;
   fp = fopen("/home/pracinf/stefan/public_html/Dydaktyka/JezProg/Slajdy/Labs05/baza_danych", "r");
-  while (znak != EOF)
-  {
+  while (znak != EOF) {
     znak = fgetc(fp);
     c = (int) znak;
     printf ("%c%d", znak, c);
-    for (i = 7; i >= 0; i--)
-    {
+    for (i = 7; i >= 0; i--) {
       k = c >> i;
-      if(k&1)
-      {
+      if(k&1) {
         printf ("1");
       }
-      else
-      {
+      else {
         printf ("0");
       }
     }
   }
-  return 0;
 }
