80ed808301daf43d558befefbb6534dccc5aee0d
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-13 10:30:26 +0200

    Update Zad03.c

--- Lab01/Zad03.c
+++ Lab01/Zad03.c
@@ -1,3 +1,8 @@
+/*Napisać programy testujące działanie operatorów z efektami ubocznymi w różnych sytuacjach, od których może zależeć wynik działania. Np.
+  *a[i++] = 0;   oraz   a[++i] = 0;
+  *y = (x += 2);
+  *z = (x %= 3) + (x %= 4);*/
+
 #include <stdio.h>
 
 int main(){
