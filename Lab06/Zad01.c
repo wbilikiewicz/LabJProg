b8aea3201a09ba2c55b7c9579d91dec23408c75a
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-13 10:58:33 +0200

    Update Zad01.c

--- Lab06/Zad01.c
+++ Lab06/Zad01.c
@@ -1,3 +1,12 @@
+/* Tutaj jest niepełny program w C, zawierający prototypy funkcji obsługujących podstawowe działania na listach. Należy dopisać ciała tych funkcji tak, żeby
+    * te funkcje działały zgodnie z podanymi komentarzami,
+    * prawidłowo działała funkcja odwroc odwracająca listę,
+    * program główny drukował ciąg 3 słów:
+       ghi
+       def
+       abc
+Ani w definicji typu lista, ani w ciele funkcji odwroc, ani w części main programu niczego nie zmieniać -- tylko wypełnić ciała tych funkcji, których jedyną zawartością jest komentarz. Definiowane funkcje (z wyjątkiem funkcji druk) nie mogą niczego pisać ani czytać. */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -14,12 +23,10 @@ typedef struct sl {
 int pusta(lista lis) {
   // jesli  lis  jest lista pusta (wskaznik  NULL)
   // to wartoscia jest liczba rozna od 0, w przeciwnym razie 0
-if (lis == NULL)
-{
+if (lis == NULL) {
   return 1;
 }
-else
-{
+else {
   return 0;
 }
 }
