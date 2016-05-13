236124c1ca66b78983eb82b54a9d98111f1cc081
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-13 10:25:12 +0200

    Update Zad03.c

--- Lab01/Zad03.c
+++ Lab01/Zad03.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 int main(){
-  int a1[5]={1,1,1,1,1};
-  int a2[5]={1,1,1,1,1};
-  int i=0, j=0;
+  int a1[5] = {1,1,1,1,1};
+  int a2[5] = {1,1,1,1,1};
+  int i = 0, j = 0;
   a1[i++] = 0;
-  for(j=0; j<5; j++){
+  for(j = 0; j < 5; j++){
     printf("%d", a1[j]);
   }
   printf ("\n");
   a2[i++] = 0;
-  j=0; i=0;
-  for(j=0; j<5; j++){
+  j = 0; i = 0;
+  for(j = 0; j < 5; j++){
     printf("%d", a2[j]);
   }
   printf ("\n");
+  
   int y, x;
   printf("y=(x += 2)\n");
   printf("Podaj x: ");
@@ -28,6 +29,4 @@ int main(){
   scanf("%d", &c);
   z = (c % 3) + (c % 4);
   printf("Wynik: %d\n", z);
-
-return 0;
 }
