fda84d1041d98a860c09331f3b29751614fbd52f
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-13 11:04:21 +0200

    Update Zad04.c

--- Lab08/Zad04.c
+++ Lab08/Zad04.c
@@ -1,3 +1,11 @@
+/* Napisać makrodefinicję  PETLA , której wywołanie
+    PETLA(komenda1, warunek, komenda2) 
+działa tak, jak na schemacie obok (pętla while ,,zrośnięta'' z pętlą do...while).
+Na przykład wywołanie
+    a=0; b=0; i=0;
+    PETLA(a++ ,  i<5 ,  b++; i++); 
+powinno przypisywać zmiennej   a   wartość 6, a zmiennej   b   wartość 5. */
+
 #include <stdio.h>
 
 #define PETLA(komenda1, warunek, komenda2) \
