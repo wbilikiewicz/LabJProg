764a5599dc8dff3ae9607130d297f2f997313413
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-17 15:45:05 +0200

    Update Zad04.c

--- Lab08/Zad04.c
+++ Lab08/Zad04.c
@@ -6,15 +6,16 @@ Na przykład wywołanie
     PETLA(a++ ,  i<5 ,  b++; i++); 
 powinno przypisywać zmiennej   a   wartość 6, a zmiennej   b   wartość 5. */
 
+
 #include <stdio.h>
 
 #define PETLA(komenda1, warunek, komenda2) \
 komenda1; while(warunek){komenda2; komenda1; }
 
-int main(){
-char a [DUZO], x;
-int n = 0, i = 0;
-PETLA (x = getchar(), x != EOF, a[n] = x; n++);
-for (i = 0; i < n; i++);
-printf("%c\n", a[i]);
+int main()
+{
+    int a=0, b=0, i=0;
+    PETLA(a++ ,  i<5 ,  b++; i++);
+    printf("%i\n", a);
+    printf("%i\n", b);
 }
