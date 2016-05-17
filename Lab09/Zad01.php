f2d78e582a51d2f72d0a47ee977a2e348393da22
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-17 15:49:30 +0200

    Update Zad01.php

--- Lab09/Zad01.php
+++ Lab09/Zad01.php
@@ -1,42 +1,23 @@
-<html>
-<head>
-<title>Simple PHP Calculator</title>
-</head>
-<body>
-<form method='post' action='calculator.php'>
-<input type='text' name='value1'>
-<input type='text' name='value2'><select name='action'>
-<option>+</option>
-<option>-</option>
-<option>*</option>
-<option>/</option>
-</select>
-<input type='submit' name='submit' value='Calculate Now'></form>
 <?php
-if(isset($_POST['submit'])){$value1 = $_POST['value1'];
-$value2 = $_POST['value2'];
-$action = $_POST['action'];
-if($action=="+"){
-echo "<b>Your Answer is:</b><br>";
-echo $value1+$value2;
-}
-
-if($action=="-"){
-echo "<b>Your Answer is:</b><br>";
-echo $value1-$value2;
-}
-
-if($action=="*"){
-echo "<b>Your Answer is:</b><br>";
-echo $value1*$value2;
-}
-
-if($action=="/"){
-echo "<b>Your Answer is:</b><br>";
-echo $value1/$value2;
-}
-}
+  error_reporting(0);
+	$digit_one = $_POST['liczba1'];
+	$digit_two = $_POST['liczba2'];
+	$op = $_POST['znak'];
+	$result = "";
+	switch ($op)
+	{
+		case "+":
+			$result = $digit_one + $digit_two;
+			break;
+		case "-":
+			$result = $digit_one - $digit_two;
+			break;
+		case "*":
+			$result = $digit_one * $digit_two;
+			break;
+		case "/":
+			$result = $digit_one / $digit_two;
+			break;
+			}
+	echo '<br/>'.'Wynik: '.$result;
 ?>
-
-</body>
-</html>
