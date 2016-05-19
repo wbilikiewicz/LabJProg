459b302fe3562970b4e6dbfd3c9c255884c92a99
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-19 14:47:39 +0200

    Update

--- Lab09/Zad03.php
+++ Lab09/Zad03.php
@@ -1,26 +1,40 @@
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
-	<meta charset="utf-8" />
+        <meta charset="utf-8" />
 </head>
 
 <body>
 <div style="float:left; vertical-align:top;">
 
 <h2>
-Licznik odwiedzin : 
+Licznik odwiedzin :<br>
 
 <?php
-	$file= @fopen("licznik.txt", "w+");
-	fscanf($file, "%d", $count);
-	$count++;
-	rewind($file);
-	fputs($file, $count);
-	fclose($file);
-	echo $count;
+                                session_start();
+                                $counter_name = "counter.txt";
+                                if (!file_exists($counter_name)) {
+                                  $f = fopen($counter_name, "w");
+                                  fwrite($f,"0");
+                                  fclose($f);
+                                }
+
+                                $f = fopen($counter_name,"r");
+                                $counterVal = fread($f, filesize($counter_name));
+                                fclose($f);
+
+                                if(!isset($_SESSION['has_visited'])){
+                                  $_SESSION['has_visited']="yes";
+                                  $counterVal++;
+                                  $f = fopen($counter_name, "w");
+                                  fwrite($f, $counterVal);
+                                  fclose($f);
+                                }
+                                echo '<span style="font-size: 26px;">'.'Jestes odwiedzajacym nr '.$counterVal .'</span>';
 ?>
 
 </h2>
 
 </body>
 </html>
+
