8c6222936c936eb06574aff2f05e7cfac9a0d6e5
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-20 10:30:43 +0200

    Update Zad02.php

--- Lab09/Zad02.php
+++ Lab09/Zad02.php
@@ -7,6 +7,13 @@
 </head>
 <body>
 <h1> Chetni na impreze: </h1>
+<div id="container">
+	<ul id="menu">
+      <li><a href="Zad01.php">Zadanie 1</a></li>
+      <li><a href="Zad02.php">Zadanie 2</a></li>
+      <li><a href="Zad03.php">Zadanie 3</a></li>
+    </ul>
+</div> 
 <div id="style">
 	<div class = "style1">
 		<br /><br />
