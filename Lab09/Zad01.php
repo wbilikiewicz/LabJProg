07df0bc19deb72cef1b090e361289fd66abf8535
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2016-05-20 10:30:09 +0200

    Update Zad01.php

--- Lab09/Zad01.php
+++ Lab09/Zad01.php
@@ -7,6 +7,13 @@
 </head>
 <body>
 <h1> Kalkulator </h1>
+<div id="container">
+	<ul id="menu">
+      <li><a href="Zad01.php">Zadanie 1</a></li>
+      <li><a href="Zad02.php">Zadanie 2</a></li>
+      <li><a href="Zad03.php">Zadanie 3</a></li>
+    </ul>
+</div> 
 <div id="style">
 	<div class = "style1">
 	<form method="POST" action="">
@@ -39,7 +46,7 @@
             $wynik = $liczba1/$liczba2;
             break;
          }
-         echo '<br/>'.'Wynik: '.$result;
+         echo '<br/>'.'Wynik: '.$wynik;
 	?>
 </div>
 </div>
